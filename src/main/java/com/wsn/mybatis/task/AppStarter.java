17e2bc8e84c12700fd91b985e10eeb0ef66a508a
wusn <wusn@ipampas.com>
2017-12-28 10:52:08 +0800

    spring boot application

--- src/main/java/com/wsn/mybatis/task/AppStarter.java
+++ src/main/java/com/wsn/mybatis/task/AppStarter.java
@@ -1,12 +1,14 @@
 package com.wsn.mybatis.task;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author sharing
  * @version 1.0.0
  **/
+@SpringBootApplication
 public class AppStarter {
     public static void main(String[] args) {
-
 
     }
 }
